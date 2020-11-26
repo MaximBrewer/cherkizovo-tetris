e06dc84bc9b5152060fc024cec59f29e500f3abc
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-26 19:13:46 +0600

    sds

--- src/panels/Game.js
+++ src/panels/Game.js
@@ -33,6 +33,31 @@ const Game = ({ id, go, route, fetchedUser, userHasSeenIntro }) => {
 	const [canvas, setCanvas] = useState({
 		width: 288
 	})
+	var AppConstants = {
+		actions: {
+		  HARD_DROP: 'HARD_DROP',
+		  MOVE_DOWN: 'MOVE_DOWN',
+		  MOVE_LEFT: 'MOVE_LEFT',
+		  MOVE_RIGHT: 'MOVE_RIGHT',
+		  FLIP_CLOCKWISE: 'FLIP_CLOCKWISE',
+		  FLIP_COUNTERCLOCKWISE: 'FLIP_COUNTERCLOCKWISE',
+		  PAUSE: 'PAUSE',
+		  RESUME: 'RESUME',
+		  HOLD: 'HOLD'
+		},
+		states: {
+		  PAUSED: 'PAUSED',
+		  PLAYING: 'PLAYING',
+		  LOST: 'LOST'
+		},
+		events: {
+		  LINE_CLEARED: 'LINE_CLEARED',
+		  PLAYER_LOST: 'PLAYER_LOST'
+		},
+		// dimensions in "blocks"
+		GAME_WIDTH: 10,
+		GAME_HEIGHT: 10
+	  };
 
 	return (
 		<Panel id={id}>
@@ -43,7 +68,7 @@ const Game = ({ id, go, route, fetchedUser, userHasSeenIntro }) => {
 							<h4>Миссия: <br /> накрой праздничный стол с Черкизово!</h4>
 							<p>Набери больше всех баллов и&nbsp;получи крутые призы!</p>
 						</Div>
-						<Tetris GAME_HEIGHT={10}>
+						<Tetris AppConstants={AppConstants}>
 							{({ HeldPiece, Gameboard, PieceQueue, points, linesCleared }) => {
 								return (
 									<Div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>

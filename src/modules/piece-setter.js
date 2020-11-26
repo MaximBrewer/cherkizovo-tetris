374ba9286d1f6a9534eb724f019637027c5a672d
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-26 21:15:41 +0600

    sds

--- src/modules/piece-setter.js
+++ src/modules/piece-setter.js
@@ -2,6 +2,7 @@
 // the returned function will set a piece on that board
 export default function createPieceSetter(board) {
   return function pieceSetter(blocks, position, className) {
+    console.log(blocks)
     for (let x = 0; x < blocks[0].length; x++) {
       for (let y = 0; y < blocks[0].length; y++) {
         const block = blocks[y][x];

1e5e90071fd99a8b91ccca07253e85300d760cbf
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-01 19:44:59 +0600

    sdsd

--- src/panels/Game.js
+++ src/panels/Game.js
@@ -37,7 +37,7 @@ const Game = ({ id, go, route, fetchedUser, userHasSeenIntro }) => {
 		go(route)
 	});
 
-	const [tetris, setTetris] = setState(null)
+	const [tetris, setTetris] = useState(null)
 
 	useEffect(() => {
 		setTetris(getTetris())

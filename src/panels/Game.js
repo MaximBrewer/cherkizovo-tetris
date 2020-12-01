76344c44a04b926b0aa874b1716d0fa257b487c4
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-01 19:29:19 +0600

    dfdf

--- src/panels/Game.js
+++ src/panels/Game.js
@@ -29,10 +29,7 @@ const Game = ({ id, go, route, fetchedUser, userHasSeenIntro }) => {
 		AppActions.flipClockwise()
 	}
 
-	const [show, setShow] = useState(true);
-
 	PieceStore.on(events.PLAYER_LOST, () => {
-		setShow(false)
 		go(route)
 	});
 
@@ -45,56 +42,54 @@ const Game = ({ id, go, route, fetchedUser, userHasSeenIntro }) => {
 							<h4>Миссия: <br /> накрой праздничный стол с Черкизово!</h4>
 							<p>Набери больше всех баллов и&nbsp;получи крутые призы!</p>
 						</Div>
-						{show &&
-							<Tetris>
-								{({ HeldPiece, Gameboard, PieceQueue, points, linesCleared }) => {
-									return (
-										<Div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-											<Div style={{ width: "32%", justifyContent: "center", alignItems: "center" }}>1</Div>
-											<Div style={{ width: "36%", justifyContent: "center", alignItems: "center" }}>
-												<div style={{
-													backgroundPosition: "center",
-													backgroundSize: "contain",
-													backgroundImage: 'url(' + canavsBg + ')'
-												}}>
-													<div>
-														{/* <HeldPiece /> */}
-														<Gameboard />
-														{/* <PieceQueue /> */}
-													</div>
+						<Tetris>
+							{({ HeldPiece, Gameboard, PieceQueue, points, linesCleared }) => {
+								return (
+									<Div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+										<Div style={{ width: "32%", justifyContent: "center", alignItems: "center" }}></Div>
+										<Div style={{ width: "36%", justifyContent: "center", alignItems: "center" }}>
+											<div style={{
+												backgroundPosition: "center",
+												backgroundSize: "contain",
+												backgroundImage: 'url(' + canavsBg + ')'
+											}}>
+												<div>
+													{/* <HeldPiece /> */}
+													<Gameboard />
+													{/* <PieceQueue /> */}
 												</div>
+											</div>
+										</Div>
+										<Div style={{ width: "32%", justifyContent: "center", alignItems: "center", padding: "0 1rem 0 2rem" }}>
+											<Div className={`scores`}>
+												<p>Очков: {points}</p>
+												<p>Линий: {linesCleared}</p>
 											</Div>
-											<Div style={{ width: "32%", justifyContent: "center", alignItems: "center", padding: "0 1rem 0 2rem" }}>
-												<Div className={`scores`}>
-													<p>Очков: {points}</p>
-													<p>Линий: {linesCleared}</p>
-												</Div>
-												<Div style={{
-													background: "#AA2223",
-													border: "2px solid #FFFFFF",
-													boxSizing: "border-box",
-													boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-													borderRadius: "1.5rem",
-													padding: "0.5rem 1.5rem"
-												}}>
-													<Div className={`control`} style={{ display: "flex", justifyContent: "space-between" }}>
-														<a href="#" onClick={moveLeft} style={{ display: "block", width: "25%" }}>
-															<Left style={{ display: "block", width: "100%" }} />
-														</a>
-														<a href="#" onClick={rotate} style={{ display: "block", width: "25%" }}>
-															<Rotate style={{ display: "block", width: "100%" }} />
-														</a>
-														<a href="#" onClick={moveRight} style={{ display: "block", width: "25%" }}>
-															<Right style={{ display: "block", width: "100%" }} />
-														</a>
-													</Div>
+											<Div style={{
+												background: "#AA2223",
+												border: "2px solid #FFFFFF",
+												boxSizing: "border-box",
+												boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+												borderRadius: "1.5rem",
+												padding: "0.5rem 1.5rem"
+											}}>
+												<Div className={`control`} style={{ display: "flex", justifyContent: "space-between" }}>
+													<a href="#" onClick={moveLeft} style={{ display: "block", width: "25%" }}>
+														<Left style={{ display: "block", width: "100%" }} />
+													</a>
+													<a href="#" onClick={rotate} style={{ display: "block", width: "25%" }}>
+														<Rotate style={{ display: "block", width: "100%" }} />
+													</a>
+													<a href="#" onClick={moveRight} style={{ display: "block", width: "25%" }}>
+														<Right style={{ display: "block", width: "100%" }} />
+													</a>
 												</Div>
 											</Div>
 										</Div>
-									);
-								}}
-							</Tetris>
-						}
+									</Div>
+								);
+							}}
+						</Tetris>
 					</Div>
 				</Fragment>
 			}

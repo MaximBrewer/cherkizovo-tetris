e4cd46ce8e5b9169e17243d16626ccab4379e104
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-02 12:05:49 +0600

    sdsd

--- src/panels/Outro.js
+++ src/panels/Outro.js
@@ -2,6 +2,7 @@
 import React, { Fragment } from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import ScoreStore from '../stores/score-store';
 
 import './Outro.css';
 import bg from '../img/intro-bg.jpg';
@@ -10,6 +11,10 @@ import logo from '../img/logo-white.png';
 const Outro = ({ id, route, fetchedUser, go }) => {
 	const replay = () => {
 
+	}
+
+	const pad = (num) => {
+		return ('000000000' + num).substr(-6).split();
 	}
 	return (
 		<Panel id={id}>
@@ -22,11 +27,15 @@ const Outro = ({ id, route, fetchedUser, go }) => {
 							<h2>Новый год с</h2>
 							<h2><img src={logo} alt="" style={{ width: "12rem" }} /></h2>
 							<h3>Миссия: <br /> накрой праздничный стол с&nbsp;Черкизово!</h3>
-							<p>Набери больше всех баллов и&nbsp;получи крутые призы!</p>
+							<p className={`numbers`}>
+								{pad(ScoreStore.getPoints()).map(item, index => (
+									<span key={index}>{item}</span>
+								))}
+							</p>
 							<p>&nbsp;</p>
 							<p>
-								<button className={`btn-red`} onClick={() => go(route)}>Сыграть еще раз</button>&nbsp;
-								<button className={`btn-red`} onClick={() => replay}>Рассказать друзьям</button>
+								<button className={`btn-red`} onClick={() => go(route)}>Сыграть еще раз</button>&nbsp;&nbsp;
+								<button className={`btn-blue`} onClick={() => replay}>Рассказать друзьям</button>
 							</p>
 						</Div>
 					</Div>

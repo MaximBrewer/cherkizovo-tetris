bbf4efda5975c9bef25084f69119bfd84b69308d
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-02 14:23:16 +0600

    dfdf

--- src/panels/Outro.js
+++ src/panels/Outro.js
@@ -10,9 +10,7 @@ import logo from '../img/logo-white.png';
 
 const Outro = ({ id, route, fetchedUser, go, bridge }) => {
 	const replay = () => {
-		console.log(1);
-		console.log(`Я набрал ${ScoreStore.getPoints()} очков!`);
-		// console.log(bridge.send("VKWebAppShowWallPostBox", {"message": `Я набрал ${ScoreStore.getPoints()} очков!`}));
+		console.log(bridge.send("VKWebAppShowWallPostBox", {"message": `Я набрал ${ScoreStore.getPoints()} очков!`}));
 	}
 
 	const pad = (num) => {

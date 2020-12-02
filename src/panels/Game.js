5217ae1698108912d1e5d3e58b800f726f5a77b2
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-02 13:27:07 +0600

    sdsd

--- src/panels/Game.js
+++ src/panels/Game.js
@@ -20,61 +20,61 @@ const Game = ({ id, go, route, fetchedUser, activePanel }) => {
 
 
 
-    function renderView({ style, ...props }) {
-        const viewStyle = {
-            paddingRight: 26
-        };
-        return (
-            <div
-                className="box"
-                style={{ ...style, ...viewStyle }}
-                {...props} />
-        );
-    }
+	function renderView({ style, ...props }) {
+		const viewStyle = {
+			paddingRight: 26
+		};
+		return (
+			<div
+				className="box"
+				style={{ ...style, ...viewStyle }}
+				{...props} />
+		);
+	}
 
-    function renderThumbHorizontal({ style, ...props }) {
-        return (
-            <div />
-        );
-    }
+	function renderThumbHorizontal({ style, ...props }) {
+		return (
+			<div />
+		);
+	}
 
-    function renderTrackHorizontal({ style, ...props }) {
-        return (
-            <div />
-        );
-    }
+	function renderTrackHorizontal({ style, ...props }) {
+		return (
+			<div />
+		);
+	}
 
-    function renderTrackVertical({ style, ...props }) {
-        const trackStyle = {
-            width: "16px",
-            padding: "4px",
-            borderRadius: "8px",
-            backgroundColor: "#E54C2E",
-            position: "absolute",
-            right: 0,
-            bottom: 0,
-            top: 0
-        };
-        return (
-            <div
-                style={{ ...style, ...trackStyle }}
-                {...props} />
-        );
-    }
+	function renderTrackVertical({ style, ...props }) {
+		const trackStyle = {
+			width: "16px",
+			padding: "4px",
+			borderRadius: "8px",
+			backgroundColor: "#E54C2E",
+			position: "absolute",
+			right: 0,
+			bottom: 0,
+			top: 0
+		};
+		return (
+			<div
+				style={{ ...style, ...trackStyle }}
+				{...props} />
+		);
+	}
 
-    function renderThumbVertical({ style, ...props }) {
-        const thumbStyle = {
-            width: "8px",
-            height: "102px",
-            borderRadius: "4px",
-            backgroundColor: "#ffffff",
-        };
-        return (
-            <div
-                style={{ ...style, ...thumbStyle }}
-                {...props} />
-        );
-    }
+	function renderThumbVertical({ style, ...props }) {
+		const thumbStyle = {
+			width: "8px",
+			height: "102px",
+			borderRadius: "4px",
+			backgroundColor: "#ffffff",
+		};
+		return (
+			<div
+				style={{ ...style, ...thumbStyle }}
+				{...props} />
+		);
+	}
 
 	const pad = (num) => {
 		return ('000000000' + num).substr(-6).split('');
@@ -159,35 +159,29 @@ const Game = ({ id, go, route, fetchedUser, activePanel }) => {
 													mobile={true}
 												>
 													<div style={{ paddingRight: "10px" }}>
-														{questions.map((item, index) => (
-															<div key={index} className="faq-item">
-																<div onClick={(e) => openQuestion(index)} className="question">{item.question} <ArrowDown style={{ fill: "#ffffff", stroke: "#ffffff", width: "17px", height: "11px" }} /></div>
-																<div className="answer" style={{ maxHeight: item.isOpen ? "100%" : "0" }}><div>{parse(item.answer)}</div></div>
-															</div>
-														))}
+														<ol>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+															<li>Михаил: Счет <strong>678 800</strong></li>
+														</ol>
 													</div>
 												</Scrollbars>
-												<ol>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-													<li>Михаил: Счет <strong>678 800</strong></li>
-												</ol>
 											</Div>
 											<Div style={{
 												background: "#AA2223",

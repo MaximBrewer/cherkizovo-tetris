4b2ba431c6ca3bb281ecb6caf57e82819350f0bf
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-03 01:39:23 +0600

    sds

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -1,36 +1,12 @@
 import React from 'react';
-import key from 'keymaster';
 import AppActions from '../actions/app-actions';
 import GameStore from '../stores/game-store';
-// import AppConstants from '../constants/app-constants';
-
-// const { states } = AppConstants;
 
 function gameBoard() {
   return {
     gameBoard: GameStore.getGameBoard()
   };
 }
-
-const keyboardMap = {
-  // x: AppActions.moveDown,
-  left: AppActions.moveLeft,
-  right: AppActions.moveRight,
-  space: AppActions.hardDrop,
-  // z: AppActions.flipCounterclockwise,
-  // x: AppActions.flipClockwise,
-  up: AppActions.flipClockwise,
-  // s: AppActions.flipClockwise,
-  // p: () => {
-  //   if (GameStore.getCurrentState() === states.PLAYING) {
-  //     AppActions.pause();
-  //   } else {
-  //     AppActions.resume();
-  //   }
-  // },
-  // c: AppActions.hold,
-  // shift: AppActions.hold
-};
 
 const watchKeys = (e) => {
   if (e.isComposing || e.keyCode === 229) {
@@ -60,39 +36,13 @@ const watchKeys = (e) => {
     default:
       break;
   }
-  // Object.keys(keyboardMap).forEach((k) => {
-  //   if(e.)
-  // })
 }
 
 function addKeyboardEvents() {
   window.addEventListener('keydown', watchKeys)
-  // key("up", toUp);
-  // key("down", toDown);
-  // key("right", toRight);
-  // key("left", toLeft);
-  // Object.keys(keyboardMap).forEach((k) => {
-  //   // if (k === 'shift') {
-  //   //   DetectShift.bind(keyboardMap[k]);
-  //   // } else {
-  //   // key(k, keyboardMap[k]);
-  //   // }
-  //   window.addEventListener('keydown',)
-  // });
 }
 function removeKeyboardEvents() {
   window.removeEventListener('keydown', watchKeys)
-  // key.unbind("up");
-  // key.unbind("down");
-  // key.unbind("left");
-  // key.unbind("right");
-  // Object.keys(keyboardMap).forEach((k) => {
-  //   // if (k === 'shift') {
-  //   //   DetectShift.unbind(keyboardMap[k]);
-  //   // } else {
-  //   // key.unbind(k);
-  //   // }
-  // });
 }
 
 export default class Gameboard extends React.Component {
